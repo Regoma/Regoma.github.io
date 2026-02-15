2c6ddde6fc272d7c6c88535b514d5ac4115952c2
Regoma <quentinbosia@yahoo.fr>
2026-02-15 19:12:19 +0100

    add: Game/project

--- js/main.js
+++ js/main.js
@@ -12,3 +12,17 @@ document.addEventListener("DOMContentLoaded", () => {
     setInterval(changeSlide, 4000);
 
 });
+
+// PROJECT CAROUSELS
+document.querySelectorAll(".project-carousel").forEach(carousel => {
+
+    const images = carousel.querySelectorAll("img");
+    let index = 0;
+
+    setInterval(() => {
+        images[index].classList.remove("active");
+        index = (index + 1) % images.length;
+        images[index].classList.add("active");
+    }, 2500);
+
+});
